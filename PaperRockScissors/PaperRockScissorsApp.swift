2cde3cf48bdd99c46a3a67ba1c5f527855d3943f
Tony Chang <tony@eliminated.org>
2024-08-18 13:56:31 -0700

    Initial Commit

---
+++ PaperRockScissors/PaperRockScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperRockScissorsApp.swift
+//  PaperRockScissors
+//
+//  Created by Tony Chang on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperRockScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
